3e410f0f8af9266c1c7419e675e16ef5e64857cc
Adrian Vrinceanu <logos@head.local>
2020-01-25 18:16:19 +0100

    what

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+bot.on(`message`, (message) => {
+
+  if(message.content.toLowerCase() === 'f' || message.content.toLowerCase() === 'ff' || message.content.toLowerCase() === 'fff' ){
+    //message.reply('@here');
+    message.channel.send('@here war fund');
+  }
+});
+
+
+// login bot
+bot.login('NjcwNTk5NDE3MzQyMTk3Nzkx.Xixm9A.p3r70KBNUZH0hY0tYg0yDbW3Irs');
